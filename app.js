1b919125cd2fe745d950990d2b24fa9a2c0245a4
Hunain Afzal <hunainafzal.238@gmail.com>
2023-04-01 13:02:03 +0500

    New

---
+++ app.js
@@ -0,0 +1,127 @@
+
+// chapter 1;
+
+alert("hello world")
+alert("Error! plese enter your vaild password")
+alert("Welcome to js land..." + "\n"+"Happy coding")
+alert("Welcome to js land..")
+alert("Happy coding."+"\n"+ "Prevent this page from creating additional diologe.")
+console.log("Hello... I can run js through web browser's console")
+
+// chapter 2;
+
+var a = +prompt("enter your age", "your age ")
+var b = a + 10
+console.log(b)
+
+var username; 
+var myname = "Hunain Afzal";
+var massage = "hello world";
+alert(massage)
+
+var name = "Hunain Afzal";
+alert(name);
+
+var age = 18;
+alert(age);
+
+var roll = "certified web devloper";
+alert(roll)
+
+var pizza = "pizza"+"\n"+"pizz"+"\n"+"piz"+"\n"+"pi"+"\n"+"p";
+alert(pizza);
+
+var email = "My email is hunainarain.026@gmail.com";
+alert(email)
+
+var book = "I am learning from A smarter way to learn javascript";
+alert(book);
+
+var content = "yah! I can write HTML content through javascript"
+document.write(content);
+
+var end = "*_________^END^__________*"
+alert(end);
+
+// chapter 3;
+
+var age = "I'm 18 years old"
+alert(age)
+
+var visit = "You have visited this site 1 times"
+alert(visit)
+
+var visit = "You have visited this site 2 times"
+alert(visit)
+
+var visit = "You have visited this site 3 times"
+alert(visit)
+
+var visit = "You have visited this site 4 times"
+alert(visit)
+
+var visit = "You have visited this site 5 times"
+alert(visit)
+
+var visit = "You have visited this site 6 times"
+alert(visit)
+
+var visit = "You have visited this site 7 times"
+alert(visit)
+
+var visit = "You have visited this site 8 times"
+alert(visit)
+
+var visit = "You have visited this site 9 times"
+alert(visit)
+
+var visit = "You have visited this site 10 times"
+alert(visit)
+
+var visit = "You have visited this site 11 times"
+alert(visit)
+
+var visit = "You have visited this site 12 times"
+alert(visit)
+
+var visit = "You have visited this site 13 times"
+alert(visit)
+
+var visit = "You have visited this site 14 times"
+alert(visit)
+
+var visit = "You have visited this site 15 times"
+alert(visit)
+
+var visit = "You have visited this site 16 times"
+alert(visit)
+
+var visit = "You have visited this site 17 times"
+alert(visit)
+
+var visit = "You have visited this site 18 times"
+alert(visit)
+
+var visit = "You have visited this site 19times"
+alert(visit)
+
+var visit = "You have visited this site 20 times"
+alert(visit)
+
+var visit = "You have visited this site 21 times"
+alert(visit)
+
+var visit = "You have visited this site 22 times"
+alert(visit)
+
+var visit = "You have visited this site 23 times"
+alert(visit)
+
+var visit = "You have visited this site 24 times"
+alert(visit)
+
+var birthyear = "my birth year is 2004 "
+document.write(birthyear);
+
+var data = "data type of my declaired veriable is numbrer"
+document.write(data);
